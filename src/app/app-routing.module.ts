5ec786aba7ff1d9cdfaa80379e40c5945d238ee0
toufiq-austcse <toufiq-austcse@gmail.com>
2020-09-06 10:30:24 +0600

    Changes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {DashboardguardService} from './guards/dashboardguard.service';
-import {ComparsionComponent} from "./comparsion/comparsion.component";
-import { NotiAllComponent } from './noti-all/noti-all.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AnalyticsComponent } from './analytics/analytics.component';
-import { UsersComponent } from './users/users.component';
-import { LoadingComponent } from './loading/loading.component';
-import { SenderComponent } from './inappmessage/sender/sender.component';
+import { ComparsionComponent } from "./comparsion/comparsion.component";
 import { AllComponent } from './inappmessage/all/all.component';
-import { DetailsComponent } from './inappmessage/details/details.component';
-import {LoginComponent} from "./login/login.component";
+import { LoginComponent } from "./login/login.component";
+import { NotiAllComponent } from './noti-all/noti-all.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {path: '', component: AllComponent},
